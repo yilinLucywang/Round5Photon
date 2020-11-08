306f0c1def33df311c9332dacf447f126fd70275
Yilin Wang <lwang@smith.edu>
2020-11-08 08:57:51 -0500

    Hot chick project with matchmaking functions

---
+++ HotChickPhoton/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,39 @@
+﻿//stunned; items; status
+
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*documentation: https://doc.photonengine.com/en-us/pun/current/getting-started/pun-intro 
+  scriptingAPI: https://doc-api.photonengine.com/en/pun/v2/index.html*/
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster(){
+        base.OnConnectedToMaster();
+    	//Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
